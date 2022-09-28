f0dc1fb1f85251004ec6df330d27ec55f5f47ef0
Oluwatobi Omoniyi <oluwatobi.omoniyi@bp.com>
2022-09-28 16:21:58 +0100

    Changes

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,12 +2,17 @@
 
 // Here, we create our main function.
 function fizzbuzz() {
-    
+
     console.log('Welcome to FizzBuzz!');
 
     // Put your code here...
+    for (var i=1; i < 101; i++){
+        if (i % 15 == 0) console.log("FizzBuzz");
+        else if (i % 3 == 0) console.log("Fizz");
+        else if (i % 5 == 0) console.log("Buzz");
+        else console.log(i);
+    }
 
-    
 }
 
 // Now we run the main function...
